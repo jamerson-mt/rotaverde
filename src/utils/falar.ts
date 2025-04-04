040b5b6f47447e0889817d9f718f58f886781c10
Thomaz Rodrigues <thomazrodrigues668@gmail.com>
2025-04-03 22:20:14 -0300

    chore(falar): parar a falar ao ser chamada novamente

--- src/utils/falar.ts
+++ src/utils/falar.ts
@@ -1,12 +1,19 @@
 import { nextPage } from "./nextPage";
 
 let cc = 0;
+let currentAudio: HTMLAudioElement | null = null;
 
 export function falar(audio1: string, audio2: string, rota: string) {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+    currentAudio = null;
+  }
+
   if (cc === 0) {
     const audioPath = `/audio/${audio1}.wav`;
-    const audio = new Audio(audioPath);
-    audio.play();
+    currentAudio = new Audio(audioPath);
+    currentAudio.play();
     cc = 1;
   } else {
     nextPage(rota, audio2);
