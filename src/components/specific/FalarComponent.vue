352cbaa2fe139ec3a7d9bc3a5fc03ba1f4f23741
Thomaz Rodrigues <thomazrodrigues668@gmail.com>
2025-10-05 12:47:07 -0300

    chore(ativ): silabas e letras

--- src/components/specific/FalarComponent.vue
+++ src/components/specific/FalarComponent.vue
@@ -58,12 +58,12 @@ export default defineComponent({
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.5);
+  background: rgba(0, 0, 0, 0.2);
   display: flex;
   justify-content: center;
   align-items: center;
   z-index: 1000;
-  pointer-events: auto; /* Permite interação apenas com a sobreposição */
+  pointer-events: auto; 
 }
 
 .audio-content {
