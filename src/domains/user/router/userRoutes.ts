056f4777e3ab4fcbc16d3374dcdcf696409029b6
jjamersonmt <jjamersonmt@gmail.com>
2025-11-14 18:34:11 -0300

    feat: atividades em modulos separados

--- src/domains/user/router/userRoutes.ts
+++ src/domains/user/router/userRoutes.ts
@@ -1,42 +1,61 @@
-import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomeView.vue';
-import PerfilPage from '../views/Perfil.vue';
-import Modulo from '../views/CategoriasAtividades.vue';
-import RoadMap from '../views/RoadMap.vue';
-import ShowContents from '@/domains/active/user/views/contents/ShowContents.vue';
+import { RouteRecordRaw } from "vue-router";
+import HomePage from "../views/HomeView.vue";
+import PerfilPage from "../views/Perfil.vue";
+import Modulo from "../views/CategoriasAtividades.vue";
+import RoadMap from "../views/RoadMap.vue";
+import RoadMap1 from "../views/RoadMapLetras.vue";
+import RoadMap2 from "../views/RoadMapAtividade.vue";
+import RoadMap3 from "../views/RoadMapPalavra.vue";
+import RoadMap4 from "../views/RoadMapFrase.vue";
+import ShowContents from "@/domains/active/user/views/contents/ShowContents.vue";
 
 const userRoutes: Array<RouteRecordRaw> = [
-    {
-        path: '/home',
-        name: 'Home',
-        component: HomePage
-    },
-    {
-        path: '/perfil',
-        name: 'Perfil',
-        component: PerfilPage
-    },
-    // {
-    //     path: '/att/modulo',
-    //     name: 'Modulo de atividade',
-    //     component: Modulo
-    // }, 
-    {
-        path: '/categorias',
-        name: 'Modulo de atividade',
-        component: Modulo
-    },
-    {
-      path: '/att/roadmap',
-      name: 'RoadMap',
-      component: RoadMap
-    },
-
-    
-
-    
-    { path: "/user/contents/m/:id", component: ShowContents },
-    
-]
+  {
+    path: "/home",
+    name: "Home",
+    component: HomePage,
+  },
+  {
+    path: "/perfil",
+    name: "Perfil",
+    component: PerfilPage,
+  },
+  // {
+  //     path: '/att/modulo',
+  //     name: 'Modulo de atividade',
+  //     component: Modulo
+  // },
+  {
+    path: "/categorias",
+    name: "Modulo de atividade",
+    component: Modulo,
+  },
+  {
+    path: "/att/roadmap",
+    name: "RoadMap",
+    component: RoadMap,
+  },
+  {
+    path: "/att/roadmapletras",
+    name: "RoadMap1",
+    component: RoadMap1,
+  },
+  {
+    path: "/att/roadmapatividade",
+    name: "RoadMap2",
+    component: RoadMap2,
+  },
+  {
+     path: "/att/roadmappalavra",
+    name: "RoadMap3",
+    component: RoadMap3,
+  },
+  {
+     path: "/att/roadmapfrase",
+    name: "RoadMap4",
+    component: RoadMap4,
+  },
+  { path: "/user/contents/m/:id", component: ShowContents },
+];
 
 export default userRoutes;
---
+++ src/domains/user/views/RoadMapPalavra.vue
@@ -0,0 +1,117 @@
+<script setup lang="ts">
+import { IonPage, IonContent } from "@ionic/vue";
+import Header from "@/domains/reasoning/components/HeaderTop.vue";
+import Exercise from "@/domains/portuguese/components/Exercise.vue";
+import TitleCategories from "../components/TitleCategories.vue";
+import { falar } from "@/utils/falar";
+
+const go = (rota: string) => {
+  window.location.href = rota;
+};
+</script>
+
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <div>
+        <Header />
+      </div>
+
+      <TitleCategories title="Português" route="/categorias" />
+
+      <div class="container">
+       
+
+       
+        <div
+          id="comp3"
+          class="component component3"
+          @click="falar('cacapalavras', 'cacapalavras2', 'pt/cacapalavras')"
+        >
+          <Exercise
+            img="img/roadMap/book.png"
+            title="Caça palavras"
+            link="pt/cacapalavras"
+          />
+        </div>
+        <div id="comp4" class="component component4">
+          <Exercise
+            img="img/roadMap/horse.png"
+            title="Sílabas"
+            link="att/silabas"
+            @click="go('att/silabas')"
+          />
+        </div>
+       
+      
+      
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin-top: 2rem;
+  padding: 0rem;
+  z-index: 0;
+  overflow-x: auto;
+  overflow-y: auto;
+  width: 100%;
+  height: 500px;
+  scroll-behavior: smooth;
+  white-space: nowrap;
+  scrollbar-color: #28a745 #f5f5f5;
+  scrollbar-width: thin;
+}
+
+.container::-webkit-scrollbar {
+  height: 8px; /* Altura do scroll horizontal */
+}
+
+.container::-webkit-scrollbar-thumb {
+  background-color: #28a745; /* Cor do scroll */
+  border-radius: 10px; /* Bordas arredondadas */
+}
+
+.container::-webkit-scrollbar-track {
+  background: #f5f5f5; /* Cor do fundo do scroll */
+}
+
+.component,
+.component .component {
+  flex: 0 0 auto; /* Garante que os componentes não encolham ou cresçam */
+  width: 150px; /* Define uma largura fixa para os componentes */
+  height: 150px; /* Define uma altura fixa para os componentes */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #e0f7e9; /* Cor de fundo verde claro */
+  border-radius: 10px; /* Bordas arredondadas */
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Adiciona sombra */
+  transition: transform 0.3s ease, box-shadow 0.3s ease; /* Animações */
+}
+
+.component1:hover,
+.component2:hover,
+.component3:hover {
+  transform: scale(1.05); /* Aumenta o tamanho ao passar o mouse */
+  box-shadow: 0 6px 10px rgba(0, 0, 0, 0.15); /* Intensifica a sombra */
+  background-color: #13457e;
+  color: #e0f7e9;
+}
+
+#comp,
+#comp1,
+#comp3 {
+  text-align: center;
+  cursor: pointer;
+  background-color: #3e5a7a;
+}
+</style>
